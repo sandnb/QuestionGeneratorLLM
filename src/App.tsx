3af4b50d0d5e6d2cd52ab73532082996e0996fa8
sandnb <nithinsandilyab@gmail.com>
2024-05-11 12:40:53 +0530

    added index.css into app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import './App.css';
+import './index.css';
 import React, { useState } from 'react';
 function App() {
   const [query, setQuery] = useState<string>('');
